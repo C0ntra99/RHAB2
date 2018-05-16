372a43f084a0dafe16c80fe75cba20c755fb50b8
White <JW0326806@admin.rose.cc.ok.us>
2018-05-16 12:53:51 -0500

    Don't blink, please. Kill me

--- MainPi/sensehat.py
+++ MainPi/sensehat.py
@@ -109,9 +109,9 @@ def measurement_blink(sleepTime=0.5, justOnce=False):
 			sense.set_pixel(7,7,[255,0,0])
 			on = True
 		time.sleep(sleepTime)
-	sense.set_pixel(7,7,[0,0,0])
-	time.sleep(sleepTime)
 	sense.set_pixel(7,7,[255,0,0])
+	time.sleep(sleepTime)
+	sense.set_pixel(7,7,[0,0,0])
 	
 def camera_blink(sleepTime=0.5, justOnce=False):
 	def stop():
