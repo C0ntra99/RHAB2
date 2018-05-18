174fbea2fcaf874f84b75704c64efaaf1eae1b81
C0ntra99 <fowlerethan99@gmail.com>
2018-05-17 21:00:18 -0500

    Added beeper to camera

---
+++ Camera/beeper.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(22, GPIO.OUT)
+
+def beep():
+    while True:
+        GPIO.output(22, True)
+        time.sleep(5)
+        GPIO.output(22, False)
+
+beep()
